bc35d8292cf0fd96001b3119bbea582e0dff03de
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-07-30 16:20:40 -0300

    criando o componente de formlogin

--- BurgerBots/src/app/app-routing.module.ts
+++ BurgerBots/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginModule } from './view/login/login.module';
+import { HomeModule } from './view/home/home.module';
 
 const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./view/home/home.module').then(m => HomeModule)
+  },
   {
     path: 'logar',
     loadChildren: () => import('./view/login/login.module').then(m => LoginModule)
--- BurgerBots/src/app/view/login/login.module.ts
+++ BurgerBots/src/app/view/login/login.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { FormloginComponent } from './formlogin/formlogin.component';
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    FormloginComponent
   ],
   imports: [
     CommonModule,
