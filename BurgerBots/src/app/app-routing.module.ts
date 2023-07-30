caf8c789a7bbcd819232756b6900d59747ab1c32
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-07-30 15:34:57 -0300

    criando o componente de login

--- BurgerBots/src/app/app-routing.module.ts
+++ BurgerBots/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginModule } from './view/login/login.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'logar',
+    loadChildren: () => import('./view/login/login.module').then(m => LoginModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
