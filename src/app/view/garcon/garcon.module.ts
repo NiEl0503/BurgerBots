57df16a83ed2a66198236319b7cb9ed9581278a9
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-08-07 12:24:02 -0300

    criação de componente produto

--- src/app/view/garcon/garcon.module.ts
+++ src/app/view/garcon/garcon.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { GarconRoutingModule } from './garcon-routing.module';
 import { GarconComponent } from './garcon.component';
+import { ProductsComponent } from './products/products.component';
 
 
 @NgModule({
   declarations: [
-    GarconComponent
+    GarconComponent,
+    ProductsComponent
   ],
   imports: [
     CommonModule,
