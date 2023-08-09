ab216c78b947f63162c31400fbcd5519ecec6872
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-08-09 16:44:35 -0300

    trabalhando nas funcoes de products

--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private readonly URL: string = "http://localhost:8080";
 
-  constructor() { }
+  constructor(private readonly http: HttpClient) { }
+
+  getProducts(headers: HttpHeaders): Observable<any> {
+    const options = { headers: headers };
+    return this.http.get(`${this.URL}/products`, options);
+  }
 }
