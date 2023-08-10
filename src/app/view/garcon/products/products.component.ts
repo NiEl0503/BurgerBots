09eb7c10705d8613d890f4515a9e073d6887e406
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-08-10 16:09:50 -0300

    consertando um bug

--- src/app/view/garcon/products/products.component.ts
+++ src/app/view/garcon/products/products.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../../services/product/product.service';
+import { HttpHeaders } from '@angular/common/http';
 import { LocalStorageService } from '../../../services/localStorage/local-storage.service';
 
-
 @Component({
+  // selector: 'app-products-garcon',
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
@@ -12,15 +13,18 @@ export class ProductsComponent implements OnInit {
 
   products: any[] = [];
 
-  constructor(private productService: ProductService, private localStorageService: LocalStorageService) { }
+  constructor(private productService: ProductService,
+    private localStorageService: LocalStorageService
+    ) { }
 
-  ngOnInit() {
-    const accessToken = this.localStorageService.getItem('accessToken');
-    
-    if (accessToken) {
-      this.productService.getProducts().subscribe(data => {
-        this.products = data;
-      });
+    ngOnInit() {
+      const accessToken = this.localStorageService.getItem('accessToken');  
+      if (accessToken) {
+        const headers = new HttpHeaders().set('Authorization', `Bearer ${accessToken}`);
+        this.productService.getProducts().subscribe(data => {
+          this.products = data;
+          
+        });
+      }
     }
   }
-}
