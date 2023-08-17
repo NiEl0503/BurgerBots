281c908cfdaa382f97edd1d41d347af999d8923f
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-08-17 13:53:17 -0300

    anadiendo elementos para header commons

--- src/app/view/commons/header/header.component.ts
+++ src/app/view/commons/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  isGarconPage(): boolean {
+    const currentUrl = this.router.url;
+    return this.router.url.includes('/garcom') || currentUrl.includes('/cozi') || currentUrl.includes('/admin');
+  }
 }
