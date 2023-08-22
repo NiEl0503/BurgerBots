2ff4511bdbabcf6b0466aba9b45a3dec2f09a286
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-08-22 14:35:21 -0300

    trabalhando em pedidos recebidos na cozinha

--- src/app/view/cozinha/order/order.component.ts
+++ src/app/view/cozinha/order/order.component.ts
@@ -1,10 +1,31 @@
-import { Component, Input  } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { OrderService } from '../../../services/orderService/orden-service.service';
+import { ProductsCommunicationService } from '../../../services/products-communication/products-communication.service';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.css']
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
   @Input() orders: any;
+
+  constructor(
+    private orderService: OrderService,
+    private productsCommunicationService: ProductsCommunicationService
+  ) {}
+
+  ngOnInit() {
+    this.loadOrders();
+
+    this.productsCommunicationService.newOrder$.subscribe(newOrder => {
+      this.orders.push(newOrder);
+    });
+  }
+
+  private loadOrders() {
+    this.orderService.getOrders().subscribe(data => {
+      this.orders = data;
+    });
+  }
 }
