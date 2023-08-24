2afafaa4c5c656866e9fb8e0d41f473d7362af78
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-08-24 10:31:28 -0300

    criando os componentes do usuário e do produto dentro da administração

--- src/app/view/administracao/administracao.module.ts
+++ src/app/view/administracao/administracao.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AdministracaoRoutingModule } from './administracao-routing.module';
 import { AdministracaoComponent } from './administracao.component';
+import { UsersComponent } from './users/users.component';
+import { ProdutosComponent } from './produtos/produtos.component';
 
 
 @NgModule({
   declarations: [
-    AdministracaoComponent
+    AdministracaoComponent,
+    UsersComponent,
+    ProdutosComponent
   ],
   imports: [
     CommonModule,
