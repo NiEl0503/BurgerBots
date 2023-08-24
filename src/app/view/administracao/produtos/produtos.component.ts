77d5ee43846818029178dc21e7c5cc4c3c938134
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-08-24 19:58:42 -0300

    trabalhando no estilo de lista de produtos

--- src/app/view/administracao/produtos/produtos.component.ts
+++ src/app/view/administracao/produtos/produtos.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../../services/product/product.service';
 
 @Component({
   selector: 'app-produtos',
   templateUrl: './produtos.component.html',
   styleUrls: ['./produtos.component.css']
 })
-export class ProdutosComponent {
+export class ProdutosComponent implements OnInit {
+  products: any[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe(products => {
+      this.products = products;
+    });
+  }
 }
