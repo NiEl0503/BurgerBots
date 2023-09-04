56491f2934966a3dcaba0a5f0368beb6587e1d4d
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-09-04 08:47:02 -0300

    continuando com os testes

--- src/app/view/commons/header/header.component.spec.ts
+++ src/app/view/commons/header/header.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
@@ -8,14 +8,29 @@ describe('HeaderComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
+      declarations: [HeaderComponent],
+      imports: [RouterTestingModule],
     });
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+   it('should not show logout button when user is not authenticated', () => {
+    localStorage.removeItem('accessToken');
+    fixture.detectChanges();
+
+    const logoutButton = fixture.nativeElement.querySelector('.logout-button');
+    expect(logoutButton).toBeFalsy();
+  });
+
+  it('should navigate to home page and clear local storage on logout', () => {
+    localStorage.setItem('accessToken', 'example-token');
+    component.logout();
+    expect(localStorage.getItem('accessToken')).toBeNull();
+    expect(localStorage.getItem('user_data')).toBeNull();
   });
 });
